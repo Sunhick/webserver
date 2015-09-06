f4ed462d1001a1b2949ca9c3125bffa13100cb6a
Sunil <Sunhick@gmail.com>
2015-09-06 00:35:59 -0600

    makefile to discover source files

--- src/include/HttpHeader.h
+++ src/include/HttpHeader.h
@@ -4,7 +4,7 @@
  * Author : Sunil bn <suba5417@colorado.edu>
  *****************************************************/
 #ifndef WEB_KIT_HTTP_HEADER_H
-#ifndef WEB_KIT_HTTP_HEADER_H
+#define WEB_KIT_HTTP_HEADER_H
 
 #include <string>
 #include <unordered_map>
@@ -24,4 +24,4 @@ namespace webkit {
   };
 }
 
-#nendif
+#endif
