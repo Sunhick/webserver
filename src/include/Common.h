90689d27a03bb7150874779f206587f1d32ba2e0
Sunil <Sunhick@gmail.com>
2015-09-16 19:59:37 -0600

    Common header for accross webserver

---
+++ src/include/Common.h
@@ -0,0 +1,11 @@
+/****************************************************
+ *  Header for http web server
+ *
+ * Author : Sunil bn <suba5417@colorado.edu>
+ *****************************************************/
+#ifndef WEB_KIT_COMMON_H
+#define WEB_KIT_COMMON_H
+
+#define TIME_OUT 10
+
+#endif
